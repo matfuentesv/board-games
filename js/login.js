801afb2a214a4a8ca93e87b82656415e41db2292
O009920 <mfuentes@forum.cl>
2024-05-20 00:45:59 -0400

    pantalla login

---
+++ js/login.js
@@ -0,0 +1,12 @@
+// login.js
+
+document.querySelector('.show-password-btn').addEventListener('click', function() {
+    const passwordInput = document.getElementById('password');
+    if (passwordInput.type === 'password') {
+        passwordInput.type = 'text';
+        this.textContent = 'Ocultar';
+    } else {
+        passwordInput.type = 'password';
+        this.textContent = 'Mostrar';
+    }
+});
