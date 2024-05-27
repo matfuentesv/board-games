00ae6ebb5b0821c9c32cff4f0bbe73d8aaa2e44b
O009920 <mfuentes@forum.cl>
2024-05-26 20:52:45 -0400

    correccion de diseño

--- js/scripts.js
+++ js/scripts.js
@@ -207,33 +207,3 @@ window.onclick = function(event) {
 }
 
 
-
-let currentSlide = 0;
-
-function showSlide(index) {
-    const slides = document.querySelectorAll('.carousel-item');
-    if (index >= slides.length) {
-        currentSlide = 0;
-    } else if (index < 0) {
-        currentSlide = slides.length - 1;
-    } else {
-        currentSlide = index;
-    }
-    slides.forEach((slide, i) => {
-        slide.style.transform = `translateX(-${currentSlide * 100}%)`;
-    });
-}
-
-function nextSlide() {
-    showSlide(currentSlide + 1);
-}
-
-function prevSlide() {
-    showSlide(currentSlide - 1);
-}
-
-document.addEventListener('DOMContentLoaded', () => {
-    showSlide(currentSlide);
-    setInterval(nextSlide, 3000); // Cambiar de slide cada 3 segundos
-});
-
